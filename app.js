354a0e1c01b4328ab75502a58978448ed382fbdc
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2013-04-10 02:21:23 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , socketio = require('socket.io')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hjs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var server = http.createServer(app)
+  , io = socketio(server);
+
+io.on('connection', function(socket) {
+  console.log('!connection');
+
+  socket.on('foo', function(data, callback) {
+    console.log('foo:', data);
+    callback({hello: 'client'});
+  });
+});
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
